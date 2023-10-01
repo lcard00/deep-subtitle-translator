1264e4db39c06d0774ee74bf5885dbbae3485414
Leonardo Cardoso <lcard00@outlook.com>
2023-10-01 18:56:10 -0300

    inclusao de metodos utils.py

--- src/self-translator.py
+++ src/self-translator.py
@@ -1,11 +1,15 @@
-import os
-import asyncio
 import utils
 
 root_dir = "/home/lcard00/.torrents/courses/[Udemy] - The Complete 2023 Web Development Bootcamp/"
 ext_in, ext_out = "_en.srt", "_ptbr.srt"
 ignore_list = ["external_links.txt"]
 
+api_key = "bd4a51a3ab8b4bf0b9153f0751727909"
+region = "brazilsouth"
+source = "en"
+target = "pt"
+
+
 files = utils.check_files(root_dir, ext_in, ignore_list)
 for file in files:
-    utils.split_srt(file, ext_in, ext_out)
+    utils.translate_and_save(api_key, region, source, target, file, ext_in, ext_out)
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,15 @@
-import asyncio
 import os
+import time
 import traceback
 from multiprocessing.pool import ThreadPool
 
-from deep_translator import GoogleTranslator
+from deep_translator import MicrosoftTranslator
 
 part_num = 1
-base_path = ""
+_api_key = ""
+_region = ""
+_source = ""
+_target = ""
 
 
 def read_list(file, list):
@@ -35,20 +38,6 @@ def check_files(root_dir, ext_in, ignore_list):
         traceback.print_exc()
 
 
-async def multi(param):
-    await asyncio.sleep(0.01)
-    subprocess = []
-    pool = ThreadPool(processes=19)
-
-    try:
-        paralell_result = pool.apply_async(load_file)
-        subprocess.append(paralell_result)
-        paralell_list = [result.get(timeout=120) for result in subprocess]
-        return paralell_list
-    except:
-        traceback.print_exc()
-
-
 def file_output(path, ext_in, ext_out):
     try:
         return path.replace(ext_in, ext_out)
@@ -56,16 +45,26 @@ def file_output(path, ext_in, ext_out):
         traceback.print_exc()
 
 
+def base_path(base_path, ext_in=None):
+    try:
+        result = base_path.path.replace(ext_in, "")
+    except:
+        traceback.print_exc()
+
+    return result
+
+
 def split_srt(input_file, ext_in, ext_out, max_phrases=50, max_chars=5000):
     try:
         with open(input_file.path, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
+        global part_num
         part_num = 1
         phrase_count = 0
         output_file = None
 
-        base_name = input_file.path.replace(ext_in, "")
+        base_name = base_path(input_file, ext_in)
 
         for line in lines:
             if not output_file or (
@@ -73,8 +72,10 @@ def split_srt(input_file, ext_in, ext_out, max_phrases=50, max_chars=5000):
             ):
                 if output_file:
                     output_file.close()
+
                 output_filename = f"{base_name}_{part_num}.tmp"
                 output_file = open(output_filename, "w", encoding="utf-8")
+
                 part_num += 1
                 phrase_count = 0
 
@@ -83,7 +84,75 @@ def split_srt(input_file, ext_in, ext_out, max_phrases=50, max_chars=5000):
             if line.strip() == "":
                 phrase_count += 1
 
-        if output_file:
+        if input_file:
             output_file.close()
+    except:
+        traceback.print_exc()
+
+
+def do_it(input_file, max_phrase=50, max_chars=5000):
+    time.sleep(0.500)
+
+    translator = MicrosoftTranslator(
+        api_key=_api_key,
+        region=_region,
+        source=_source,
+        target=_target,
+    )
+    try:
+        with open(input_file, "r", encoding="utf-8") as f:
+            translated = translator.translate_batch(f.readlines())
+            os.remove(f.name)
+        return translated
+
+    except:
+        traceback.print_exc()
+
+
+def list_files(file, ext_in):
+    base_name = base_path(file, ext_in)
+    temp_files = []
+
+    for i in range(1, part_num):
+        file_out = str(f"{base_name}_{i}.tmp")
+
+        with open(file_out, "r", encoding="utf8") as f:
+            if type(f) is not None:
+                temp_files.append(f.name)
+
+    return temp_files
+
+
+def translate_and_save(
+    api_key, region, source, target, input_file, ext_in="_en.srt", ext_out="_ptbr.srt"
+):
+    try:
+        global _api_key
+        global _region
+        global _source
+        global _target
+
+        _api_key = api_key
+        _region = region
+        _source = source
+        _target = target
+
+        pool = ThreadPool(processes=10)
+
+        base_name = base_path(input_file, ext_in)
+        translated_filename = base_name + ext_out
+
+        if not os.path.exists(translated_filename):
+            split_srt(input_file, ext_in, ext_out)
+            temp_files = list_files(input_file, ext_in)
+
+            translated_parts = pool.map(do_it, temp_files)
+
+            with open(translated_filename, "w", encoding="utf-8") as f:
+                if type(f) is not None:
+                    for part in translated_parts:
+                        f.write("".join(part))
+            f.close()
+
     except:
         traceback.print_exc()
