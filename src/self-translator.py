9503420ff424613e07f2b974ccfecd8dc224f596
Leonardo Cardoso <lcard00@outlook.com>
2023-10-01 23:02:17 -0300

    add root_dir to .envrc file

--- src/self-translator.py
+++ src/self-translator.py
@@ -4,14 +4,14 @@ import utils
 
 load_dotenv()
 
-root_dir = "/home/lcard00/.torrents/courses/[Udemy] - The Complete 2023 Web Development Bootcamp/"
-ext_in, ext_out = "_en.srt", "_ptbr.srt"
-ignore_list = ["external_links.txt"]
-
+root_dir = os.getenv("root_dir")
 api_key = os.getenv("api_key")
 region = os.getenv("region")
 source = "en"
 target = "pt"
+ext_in ext_out = "_en.srt", "_ptbr.srt"
+ignore_list = ["external_links.txt"]
+
 # translator types:
 # 1 - Microsoft
 # 2 - Google Translator
@@ -19,5 +19,4 @@ translator = 2
 
 files = utils.check_files(root_dir, ext_in, ignore_list)
 for file in files:
-
     utils.translate_and_save(translator, ,api_key, region, source, target, file, ext_in, ext_out)
